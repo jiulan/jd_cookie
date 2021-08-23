89e30745ad0cf45cb54afca159700e1fbfc92f82
jiulan <1454119289@qq.com>
2021-08-24 00:31:22 +0800

    修复获取ck

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function step1() {
           'https://plogin.m.jd.com/login/login?appid=300' +
           `&returnurl=https://wq.jd.com/passport/LoginRedirect?state=${timeStamp}` +
           '&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
-      'User-Agent': JD_UA,
+      'User-Agent': 'jdapp;android;10.0.2;10;network/wifi;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36P',
       Host: 'plogin.m.jd.com',
     },
   });
@@ -161,7 +161,7 @@ async function step2(cookiesObj) {
           'https://plogin.m.jd.com/login/login?appid=300' +
           `&returnurl=https://wqlogin2.jd.com/passport/LoginRedirect?state=${timeStamp}` +
           '&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
-      'User-Agent': JD_UA,
+      'User-Agent': 'jdapp;android;10.0.2;10;network/wifi;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36P',
       Host: 'plogin.m.jd.com',
     },
   });
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-node /*
+/*
  * @Author: Jerrykuku https://github.com/jerrykuku
  * @Date: 2021-1-8
  * @Version: v0.0.2
@@ -140,7 +140,7 @@ async function step1() {
           'https://plogin.m.jd.com/login/login?appid=300&returnurl=https://wq.jd.com/passport/LoginRedirect?state=' +
           timeStamp +
           '&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
-        'User-Agent': JD_UA,
+        'User-Agent': 'jdapp;android;10.0.2;10;network/wifi;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36P',
         Host: 'plogin.m.jd.com',
       },
     });
@@ -184,7 +184,7 @@ async function step2() {
           'https://plogin.m.jd.com/login/login?appid=300&returnurl=https://wqlogin2.jd.com/passport/LoginRedirect?state=' +
           timeStamp +
           '&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
-        'User-Agent': JD_UA,
+        'User-Agent': 'jdapp;android;10.0.2;10;network/wifi;Mozilla/5.0 (Linux; Android 10; ONEPLUS A5010 Build/QKQ1.191014.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36P',
         Host: 'plogin.m.jd.com',
       },
     });
